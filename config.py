460532fa53c55a7589f308fd753ee1f3f13dcdea
Seclud <82933148+Seclud@users.noreply.github.com>
2024-12-16 15:38:07 +0500

    fix(conf): change secret_key

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env",env_ignore_empty=True, extra="ignore")
     DATABASE_URL: str
-    SECRET_KEY: str = "5886584bc98dd7ade1788f2322ed42e2690be2bf1f33317c18d556052fcdc8bb"
-    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8  # 8 days
+    SECRET_KEY: str
+    ALGORITHM: str
