3843ea427600c22a3566f81c723f091ebf72b927
Seclud <82933148+Seclud@users.noreply.github.com>
2025-01-11 18:14:19 +0500

    feat(front): only admin can create projects now

--- frontend/src/pages/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage.jsx
@@ -3,12 +3,14 @@ import '@mantine/core/styles.css';
 import { Badge, Button, Group, Text, Container, Paper } from '@mantine/core';
 import { useEffect, useState } from "react";
 import ProjectCreateModal from "../modals/ProjectCreateModal.jsx";
+import { useAuth } from "../contexts/AuthContext.jsx";
 
 function ProjectsPage() {
     const defaultValue = [];
     const navigate = useNavigate();
     const [projects, setProjects] = useState(defaultValue);
     const [isOpen, setIsOpen] = useState(false);
+    const auth = useAuth();
 
     const getApiData = async () => {
         const token = localStorage.getItem('authToken');
@@ -30,33 +32,39 @@ function ProjectsPage() {
     return (
         <Container>
             <Paper shadow="xs" padding="md" radius="md" style={{ backgroundColor: '#f8f9fa', width: '100%' }}>
-                {projects.map((project, index) => (
-                    <Button
-                        key={index}
-                        variant="outline"
-                        fullWidth
-                        radius="md"
-                        style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '10px', backgroundColor: 'white' }}
-                        onClick={() => navigate(`/projects/${project.id}`)}
-                    >
-                        <Group position="apart" style={{ width: '100%' }}>
-                            <div style={{ flex: 1 }}>
-                                <Text weight={500}>{project.name}</Text>
-                            </div>
-                            <div style={{ display: 'flex', alignItems: 'center' }}>
-                                <Badge color="blue" variant="light">
-                                    Начался: {project.start_date}
-                                </Badge>
-                                <Badge color="pink" variant="light" style={{ marginLeft: 10 }}>
-                                    Заканчивается: {project.end_date}
-                                </Badge>
-                            </div>
-                        </Group>
+                {projects.length === 0 ? (
+                    <Text>Вы не участвуете ни в одном проекте</Text>
+                ) : (
+                    projects.map((project, index) => (
+                        <Button
+                            key={index}
+                            variant="outline"
+                            fullWidth
+                            radius="md"
+                            style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '10px', backgroundColor: 'white' }}
+                            onClick={() => navigate(`/projects/${project.id}`)}
+                        >
+                            <Group position="apart" style={{ width: '100%' }}>
+                                <div style={{ flex: 1 }}>
+                                    <Text weight={500}>{project.name}</Text>
+                                </div>
+                                <div style={{ display: 'flex', alignItems: 'center' }}>
+                                    <Badge color="blue" variant="light">
+                                        Начался: {project.start_date}
+                                    </Badge>
+                                    <Badge color="pink" variant="light" style={{ marginLeft: 10 }}>
+                                        Заканчивается: {project.end_date}
+                                    </Badge>
+                                </div>
+                            </Group>
+                        </Button>
+                    ))
+                )}
+                {auth.user && auth.user.role !== 3 && (
+                    <Button color="blue" mt="md" radius="md" onClick={() => setIsOpen(true)}>
+                        Создать Проект
                     </Button>
-                ))}
-                <Button color="blue" mt="md" radius="md" onClick={() => setIsOpen(true)}>
-                    Создать Проект
-                </Button>
+                )}
                 <ProjectCreateModal isOpen={isOpen} setIsOpen={setIsOpen} />
             </Paper>
         </Container>
