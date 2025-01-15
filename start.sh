5510e2906f7abbffe158abc73ecece0b4b8bb4f1
Seclud <82933148+Seclud@users.noreply.github.com>
2025-01-15 21:40:45 +0500

    fix(back): trying to fix mixed content error

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo "PYTHONPATH: $PYTHONPATH"
 # python /app/app/initial_data.py
 # Start the application
 # exec uvicorn app.main:app --proxy-headers --host 0.0.0.0 --port 8000
-exec gunicorn app.main:app --worker-class uvicorn.workers.UvicornWorker --workers 4 --bind 0.0.0.0:8000
+exec gunicorn app.main:app --worker-class uvicorn.workers.UvicornWorker --workers 4 --bind 0.0.0.0:8000 --forwarded-allow-ips='*'
